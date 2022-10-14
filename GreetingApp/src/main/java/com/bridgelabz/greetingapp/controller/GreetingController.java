cd2d2860055890611fd02a8fa0e5a770ef085e18
Swayam Prakash <swayam265@gmail.com>
2022-10-14 11:19:35 +0530

    [swayam]: added greeting message with name in service layer in greeting app development

--- GreetingApp/src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ GreetingApp/src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.greetingapp.controller;
 
 import com.bridgelabz.greetingapp.entity.Greeting;
+import com.bridgelabz.greetingapp.entity.User;
 import com.bridgelabz.greetingapp.service.IGreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,19 +17,24 @@ public class GreetingController {
     private static final String template = "Hello, %s";
     private IGreetingService greetingService;
 
-    private final AtomicLong counter = new AtomicLong();
+    private final AtomicLong counter= new AtomicLong();
 
     public GreetingController(IGreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
     @GetMapping("/")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    public Greeting greeting(@RequestParam(value="name", defaultValue = "World") String name){
+        return new Greeting(counter.incrementAndGet(),String.format(template,name));
     }
 
     @GetMapping("/greeting")
-    public String greeting() {
-        return greetingService.getGreetingMessage();
+    public String greeting(@RequestParam(name = "firstName", defaultValue = "Hello") String firstName,
+                           @RequestParam(name = "lastName", defaultValue = "World") String lastName){
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        return greetingService.getGreetingMessage(user);
     }
+
 }
--- GreetingApp/src/main/java/com/bridgelabz/greetingapp/service/GreetingService.java
+++ GreetingApp/src/main/java/com/bridgelabz/greetingapp/service/GreetingService.java
@@ -1,11 +1,22 @@
 package com.bridgelabz.greetingapp.service;
 
+import com.bridgelabz.greetingapp.entity.User;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @Service
-public class GreetingService implements IGreetingService {
+public class GreetingService implements IGreetingService{
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
     public String getGreetingMessage(){
         return "Hello World!";
     }
 
+    @Override
+    public String getGreetingMessage(User user) {
+        String name = user.toString().isEmpty() ? "Hello world " : user.toString();
+        return String.format(template,name);
+    }
 }
