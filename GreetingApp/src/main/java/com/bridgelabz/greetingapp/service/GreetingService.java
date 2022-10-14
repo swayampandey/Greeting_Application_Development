eb99d5bfaaf5e59a3f5efe49bc57d3a97fe01c88
Swayam Prakash <swayam265@gmail.com>
2022-10-14 11:59:42 +0530

    [swayam]: added functionality to lis all greeting message in repository in greeting app development

--- GreetingApp/src/main/java/com/bridgelabz/greetingapp/service/GreetingService.java
+++ GreetingApp/src/main/java/com/bridgelabz/greetingapp/service/GreetingService.java
@@ -6,35 +6,42 @@ import com.bridgelabz.greetingapp.repository.GreetingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Service
-public class GreetingService implements IGreetingService{
+public class GreetingService implements IGreetingService {
     @Autowired
     private GreetingRepository greetingRepository;
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    public String getGreetingMessage(){
+    public String getGreetingMessage() {
         return "Hello World!";
     }
 
     @Override
     public String getGreetingMessage(User user) {
         String name = user.toString().isEmpty() ? "Hello world " : user.toString();
-        return String.format(template,name);
+        return String.format(template, name);
     }
 
     @Override
     public Greeting addGreetingMessage(User user) {
         String name = user.toString().isEmpty() ? "Hello world " : user.toString();
-        return greetingRepository.save(new Greeting(counter.incrementAndGet(),String.format(template,name)));
+        return greetingRepository.save(new Greeting(counter.incrementAndGet(), String.format(template, name)));
     }
+
     @Override
     public Greeting getGreetingMsgById(long id) {
         Optional<Greeting> msg = greetingRepository.findById(id);
         return msg.get();
+    }
+
+    @Override
+    public List<Greeting> getAllGreetingMsg() {
+        return greetingRepository.findAll();
     }
 }
